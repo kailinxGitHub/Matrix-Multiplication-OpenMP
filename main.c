0fc2e95c36812c4afd001a3bb56e9df4dfce9594
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-11-26 03:37:35 -0500

    fix threads_limits

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 #include <time.h>
 
@@ -74,17 +75,19 @@ double StandardMatrixMultiplication(
  * @param matrix_one the first matrix to be multiplied
  * @param matrix_two the second matrix to be multiplied
  * @param result_matrix the result of the two matrices
+ * @param threads_limits the number of threads as a limit
  * @return time taken
  */
 double OmpMatrixMultiplication(
-        int matrix_one[dim][dim],
-        int matrix_two[dim][dim],
-        int result_matrix[dim][dim]
+        int matrix_one[512][512],
+        int matrix_two[512][512],
+        int result_matrix[512][512],
+        int threads_limit
         ) {
     double start, end;
 
     start = omp_get_wtime();
-    #pragma omp parallel for num_threads(10) schedule(static) shared(matrix_one, matrix_two, result_matrix)
+    #pragma omp parallel for num_threads(threads_limit) schedule(static) shared(matrix_one, matrix_two, result_matrix)
     for (int row = 0; row < dim; row++) {
         for (int col = 0; col < dim; col++) {
             int sum = 0;
@@ -140,16 +143,20 @@ int main() {
 
     RandomIntGeneratorForMatrix(1,100,matrix_one);
     RandomIntGeneratorForMatrix(1,100,matrix_two);
-    double smm = StandardMatrixMultiplication(matrix_one, matrix_two, result_matrix);
+
+    int threads_limits[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i = 0; i < 10; i++) {
+        printf("For a thread limit of %i: ", threads_limits[i]);
+        double smm = StandardMatrixMultiplication(matrix_one, matrix_two, result_matrix);
 //    PrintMatrix(matrix_one);
 //    PrintMatrix(matrix_two);
 //    PrintMatrix(result_matrix);
-    printf("---\n");
-    double omm = OmpMatrixMultiplication(matrix_one, matrix_two, result_matrix);
+        double omm = OmpMatrixMultiplication(matrix_one, matrix_two, result_matrix, 0);
 //    PrintMatrix(matrix_one);
 //    PrintMatrix(matrix_two);
 //    PrintMatrix(result_matrix);
-    printf("---\n");
-    printf("Time difference between Standard and OpenMP matrix multiplication: %f times faster.", (double)(smm / omm));
+        printf("Time difference between Standard and OpenMP matrix multiplication: %f times faster.", (double)(smm / omm));
+        printf("\n---\n");
+    }
     return 0;
 }
